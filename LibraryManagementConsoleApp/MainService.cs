c89a67a7fbb2c7b75cb1fef9616fa9d2aad70b86
karolinaJot <amicus.karo@gmail.com>
2023-05-14 14:34:42 +0200

    add BookService (not finished) and MainService

---
+++ LibraryManagementConsoleApp/MainService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementConsoleApp
+{
+	internal class MainService
+	{
+		public string ShowMainSelection(bool showAgainMain)
+		{
+			string result = "books";
+
+			while (!showAgainMain)
+			{
+				Console.WriteLine("Welcome, in Library Management System!\nPress B to work with Books collection\nPress P to work with Borrowers collection");
+
+				ConsoleKeyInfo userResponse = Console.ReadKey();
+
+				if (userResponse.KeyChar == 'B' || userResponse.KeyChar == 'b')
+				{
+					break;
+				}
+				else if (userResponse.KeyChar == 'P' || userResponse.KeyChar == 'p')
+				{
+					result = "borrowers";
+				}
+				else
+				{
+					Console.WriteLine("\nSelection incorrect. Please, try again");
+				}
+			}
+			return result;
+		}
+	}
+}
--- LibraryManagementConsoleApp/Program.cs
+++ LibraryManagementConsoleApp/Program.cs
@@ -1,21 +1,84 @@
 ﻿
+using Azure.Identity;
 using LibraryManagementConsoleApp;
 
-BorrowersRepository borrowersRepository = new BorrowersRepository();
+
+MainService mainService = new MainService();
+BookService bookService = new BookService();
+
+bool showAgainMain = false;
+
+string selectedCollection = mainService.ShowMainSelection(showAgainMain);
+
+//work with books
+if (selectedCollection == "books")
+{
+	bool showAgainBooks = false;
+	while (!showAgainBooks)
+	{
+		Console.WriteLine("\nYou are in book section. Please select action.\nPress L to see all books in list\nPress E to edit book\nPress D to delete book\nPress A to add new book\nPress Q to quite");
+		ConsoleKeyInfo selectedAction = Console.ReadKey();
+
+		switch (selectedAction.KeyChar)
+		{
+			case 'd':
+			case 'D':
+				bookService.DeleteBook();
+				break;
+
+			case 'l':
+			case 'L':
+				bookService.DisplayBookList();
+				break;
+
+			case 'e':
+			case 'E':
+				bookService.EditBook();
+				break;
+
+			case 'a':
+			case 'A':
+				bookService.AddBook();
+				break;
+
+			case 'q':
+			case 'Q':
+				showAgainMain = true;
+				showAgainBooks = true;
+				break;
+
+			default:
+				Console.WriteLine("Incorect selection. Please, try again");
+				showAgainBooks = false;
+				break;
+		}
+	}
+}
+// work with borrowers
+else if (selectedCollection == "borrowers")
+{
+
+}
+
+//selectedCollection = mainService.ShowMainSelection(showAgainMain);
+
+
+
+//BorrowersRepository borrowersRepository = new BorrowersRepository();
 
 //Borrower borrower = new Borrower { Name = "Karolina", Email = "tojest@mail", Phone = "666777888" };
 
 //borrowersRepository.AddBorrower(borrower);
 
-Borrower[] borrowers = borrowersRepository.GetBorrowers();
+//Borrower[] borrowers = borrowersRepository.GetBorrowers();
 
-foreach (Borrower b in borrowers)
-{
+//foreach (Borrower b in borrowers)
+//{
 
-	Console.WriteLine($"{b.BorrowerId} {b.Name} {b.Email} {b.Phone} {b.TotalBorrowedBooks}");
-}
+//	Console.WriteLine($"{b.BorrowerId} {b.Name} {b.Email} {b.Phone} {b.TotalBorrowedBooks}");
+//}
 
-BooksRepository booksRepository = new BooksRepository();
+
 
 //Book book = new Book
 //{
@@ -26,13 +89,6 @@ BooksRepository booksRepository = new BooksRepository();
 
 //booksRepository.AddBook(book);
 
-Book[] books = booksRepository.GetBooks();
 
-foreach (Book b in books)
-{
-	int index = 1;
-	Console.WriteLine($"List of books: {index}. {b.Title}, {b.Author}, {b.ISBN}, {b.IsAvailable} ");
-	index++;
-}
 
-borrowersRepository.DeleteBorrower(borrowers[0].BorrowerId);
+//borrowersRepository.DeleteBorrower(borrowers[0].BorrowerId);
